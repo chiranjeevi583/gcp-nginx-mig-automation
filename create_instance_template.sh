77451fdaaa6fa68600787040215b6bef6550dee1
chiranjeevi583 <gcpchiranjeevi@gmail.com>
2024-10-11 23:17:57 +0530

    Update create_instance_template.sh

--- create_instance_template.sh
+++ create_instance_template.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+gcloud compute instance-templates create nginx-template \
+--metadata=startup-script='#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y nginx
+sudo systemctl start nginx' \
+--machine-type=e2-medium \
+--region=us-central1
 
